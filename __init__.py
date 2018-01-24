6dc1965175e07e673724408786e4dd0c0802fd53
Drew Leonard <andy.j.leonard@gmail.com>
2018-01-24 11:27:11 +0000

    Add functions to randomly choose a user, excercise and number

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import logging
+from numpy import random
 
 from opsdroid.message import Message
 from opsdroid.matchers import match_always, match_crontab
@@ -6,6 +7,21 @@ from opsdroid.matchers import match_always, match_crontab
 
 def setup(opsdroid):
     logging.debug("Loaded fitness module")
+
+
+def choose_victim(users):
+    """Randomly choose one from a list of the people in the room."""
+    return random.choice(users)
+
+
+def choose_excercise(excercise_options):
+    """Randomly select one from the list of designated exercises."""
+    return random.choice(excercise_options)
+
+
+def choose_number():
+    """Determine number of repetitions for the selected exercise."""
+    np.random.normal(loc=10, scale=5, size=100).round().astype(int)
 
 
 @match_crontab("* * * * *", timezone="Europe/London")
